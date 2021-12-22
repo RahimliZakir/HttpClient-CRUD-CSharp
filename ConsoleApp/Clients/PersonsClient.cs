77e32c908f8d923a26cdda53b126db0bb66f6f7e
RahimliZakir <zakirer@code.edu.az>
2021-12-23 03:35:55 +0400

    Add project files.

---
+++ ConsoleApp/Clients/PersonsClient.cs
@@ -0,0 +1,206 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Commands
+{
+    public class PersonsClient
+    {
+        HttpClient client = new HttpClient();
+
+        public PersonsClient(string apiLink)
+        {
+            this.client.BaseAddress = new Uri(apiLink);
+        }
+
+        async public Task GetAll(string path)
+        {
+            HttpResponseMessage getResponse = await client.GetAsync(path);
+
+            if (getResponse.IsSuccessStatusCode)
+            {
+                string data = await getResponse.Content.ReadAsStringAsync();
+
+                Console.WriteLine(data);
+            }
+            else
+            {
+                Console.WriteLine($"Xəta baş verdi: {getResponse.StatusCode}.");
+            }
+        }
+
+        async public Task GetById(string path)
+        {
+        againId:
+            Console.Write("Çağırılmasını istədiyiniz məlumatın Id-sini daxil edin: ");
+            string text = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(text))
+                goto againId;
+
+            bool result = int.TryParse(text, out int intText);
+            if (result == false)
+                goto againId;
+
+            int id = intText;
+
+            path = $"{path}/{id}";
+
+            HttpResponseMessage getResponse = await client.GetAsync(path);
+
+            if (getResponse.IsSuccessStatusCode)
+            {
+                string data = await getResponse.Content.ReadAsStringAsync();
+
+                Console.WriteLine(data);
+            }
+            else
+            {
+                Console.WriteLine($"Xəta baş verdi: {getResponse.StatusCode}.");
+            }
+        }
+
+        async public Task Add(string path)
+        {
+        nameAgain:
+            Console.Write("Person adını daxil edin: ");
+            string name = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(name))
+                goto nameAgain;
+
+            surnameAgain:
+            Console.Write("Person soyadını daxil edin: ");
+            string surname = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(surname))
+                goto surnameAgain;
+
+            ageAgain:
+            Console.Write("Person yaşını daxil edin: ");
+            string age = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(age))
+                goto ageAgain;
+
+            bool result = int.TryParse(age, out int intAge);
+            if (!result)
+                goto ageAgain;
+
+            var obj = new
+            {
+                name,
+                surname,
+                age = intAge
+            };
+
+            string serializedObj = JsonConvert.SerializeObject(obj);
+
+            StringContent content = new StringContent(serializedObj, Encoding.UTF8, MediaTypeNames.Application.Json);
+
+            HttpResponseMessage postResponse = await client.PostAsync(path, content);
+
+            if (postResponse.IsSuccessStatusCode)
+            {
+                string text = await postResponse.Content.ReadAsStringAsync();
+                Console.WriteLine("Person uğurla yaradıldı!");
+            }
+            else
+            {
+                Console.WriteLine($"Xəta baş verdi: {postResponse.StatusCode}.");
+            }
+        }
+
+        async public Task Put(string path)
+        {
+        againId:
+            Console.Write("Çağırılmasını istədiyiniz məlumatın Id-sini daxil edin: ");
+            string text = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(text))
+                goto againId;
+
+            bool result = int.TryParse(text, out int intText);
+            if (result == false)
+                goto againId;
+
+            int id = intText;
+
+            path = $"{path}/{id}";
+
+        nameAgain:
+            Console.Write("Person adını daxil edin: ");
+            string name = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(name))
+                goto nameAgain;
+
+            surnameAgain:
+            Console.Write("Person soyadını daxil edin: ");
+            string surname = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(surname))
+                goto surnameAgain;
+
+            ageAgain:
+            Console.Write("Person yaşını daxil edin: ");
+            string age = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(age))
+                goto ageAgain;
+
+            bool resultAge = int.TryParse(age, out int intAge);
+            if (resultAge != true)
+                goto ageAgain;
+
+            var person = new
+            {
+                id,
+                name,
+                surname,
+                age = intAge,
+                createdDate = "2021-12-22T22:51:11.447Z"
+            };
+
+            string serializedObj = JsonConvert.SerializeObject(person);
+
+            StringContent content = new StringContent(serializedObj, Encoding.UTF8, MediaTypeNames.Application.Json);
+
+            HttpResponseMessage putResponse = await client.PutAsync(path, content);
+
+            if (putResponse.IsSuccessStatusCode)
+            {
+                string readed = await putResponse.Content.ReadAsStringAsync();
+                Console.WriteLine("Person uğurla yeniləndi!");
+            }
+            else
+            {
+                Console.WriteLine($"Xəta baş verdi: {putResponse.StatusCode}.");
+            }
+        }
+
+        async public Task Delete(string path)
+        {
+        againId:
+            Console.Write("Çağırılmasını istədiyiniz məlumatın Id-sini daxil edin: ");
+            string text = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(text))
+                goto againId;
+
+            bool result = int.TryParse(text, out int intText);
+            if (result == false)
+                goto againId;
+
+            int id = intText;
+
+            path = $"{path}/{id}";
+
+            HttpResponseMessage deleteResponse = await client.DeleteAsync(path);
+
+            if (deleteResponse.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Seçdiyiniz məlumat uğurla silindi!");
+            }
+            else
+            {
+                Console.WriteLine($"Xəta baş verdi: ${deleteResponse.StatusCode}.");
+            }
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using ConsoleApp.Commands;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        async static Task Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            PersonsClient client = new PersonsClient("https://localhost:7198");
+
+            string path = "/api/persons";
+
+            // 1. Get Datas
+            await client.GetAll(path);
+
+            // 2. Get Data By Id
+            //await client.GetById(path);
+
+            // 3.Post Data
+            //await client.Add(path);
+
+            //4.Update Data
+            //await client.Put(path);
+
+            // 5.Delete Data
+            //await client.Delete(path);
+
+            Console.ReadKey();
+        }
+    }
+}
